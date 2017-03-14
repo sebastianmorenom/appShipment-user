815adbe8f691197d825e51fa82c51ae9424012df
Andres <Andres>
2017-03-14 00:37:34 -0500

    Added accelerometer with ripple emulator

--- src/pages/accelerometer/accelerometer.component.ts
+++ src/pages/accelerometer/accelerometer.component.ts
@@ -1,15 +1,36 @@
-import {Component, ViewChild, ElementRef} from "@angular/core";
-import { NavController } from 'ionic-angular';
-
 import {Component} from "@angular/core";
-
+import {DeviceMotion} from 'ionic-native';
 @Component({
   templateUrl: 'accelerometer.html',
 })
-
 export class Accelerometer{
-	
+	x:string;
+	y:string;
+	z:string;
+
 	constructor(){
-		
-	}	
+		this.x = "prueba";
+	}
+		getAcceleration(){
+			navigator.accelerometer.getCurrentAcceleration(this.onSuccess, this.onError);	
+		}	
+
+	onSuccess(acceleration){
+	    		console.log('Acceleration X: ' + acceleration.x + '\n' +
+	          	'Acceleration Y: ' + acceleration.y + '\n' +
+	          	'Acceleration Z: ' + acceleration.z + '\n' +
+	          	'Timestamp: '      + acceleration.timestamp + '\n');
+	          	console.log(this+"el");
+	    		this.x = acceleration.x;
+	    		this.y = acceleration.y;
+	    		this.z = acceleration.z;
+	}
+	onError() {
+    		alert('onError!');
+	}
+
 }
+
+
+
+
