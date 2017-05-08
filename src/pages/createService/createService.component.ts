4e7becbf15774816beb034a2d6a730369ae11567
sebastianmoreno <sebastianmoreno@seven4n.com>
2017-05-07 22:25:35 -0500

    Los botones de dimensiones cambian al hacer click

--- src/pages/createService/createService.component.ts
+++ src/pages/createService/createService.component.ts
@@ -5,7 +5,17 @@ import {Component} from "@angular/core";
 })
 export class CreateService {
   dimensiones:any;
+  currentDimension:number;
+
   constructor(){
-    this.dimensiones = ['light', 'dark', 'light']
+    this.dimensiones = ['light', 'primary', 'light']
+    this.currentDimension = 1;
   }
+
+  selectDimention(id:number){
+    this.dimensiones[id] = 'primary';
+    this.dimensiones[this.currentDimension] = 'light';
+    this.currentDimension = id;
+  }
+
 }
