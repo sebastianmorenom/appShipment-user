154296c316d0bea6ab9bbfda87279d1db2c2ec91
sebastianmoreno <sebastianmoreno@seven4n.com>
2017-05-14 21:51:40 -0500

    1. Se agrega alert para formulario invalido 2. Se agregan campos faltantes del mdoelo de BD

--- src/pages/createService/createService.component.ts
+++ src/pages/createService/createService.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import { AlertController } from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NavParams} from "ionic-angular";
 
@@ -11,19 +12,23 @@ export class CreateService {
   formCreateService:FormGroup;
   submitAttempt:boolean;
   user:any;
+  locations:any;
 
-  constructor(private formBuilder:FormBuilder, private navParams:NavParams){
+  constructor(private formBuilder:FormBuilder, private navParams:NavParams, private alertCtrl: AlertController){
     this.submitAttempt = false;
     this.dimensiones = ['light', 'primary', 'light'];
     this.currentDimension = 1;
     this.formCreateService = formBuilder.group({
-      toLocation: ['', Validators.compose([Validators.required])],
       toName: ['', Validators.compose([Validators.required, Validators.maxLength(30)])],
+      toIdType: ['CC', Validators.compose([Validators.required, Validators.maxLength(2)])],
+      toId: ['', Validators.compose([Validators.required, Validators.pattern("^[0-9]+$")])],
+      toNumTel: ['', Validators.compose([Validators.required, Validators.pattern("^[0-9]+$"), Validators.maxLength(10)])],
       contentDeclaration: ['', Validators.compose([Validators.required, Validators.maxLength(120)])],
       valueDeclaration: ['', Validators.compose([Validators.required, Validators.pattern("^[0-9]+$")])],
       dimension: [this.currentDimension, Validators.compose([Validators.required, Validators.pattern("^[0-9]$")])]
     });
     this.user = this.navParams.get('user');
+    this.locations = this.navParams.get('locations');
   }
 
   selectDimension(id:number){
@@ -36,13 +41,23 @@ export class CreateService {
   createService(){
     this.submitAttempt=true;
     if(!this.formCreateService.valid){
-      alert("Los campos tienen que ser diligenciados correctamente.")
+      this.presentAlert();
     }
     else {
       alert("Creating service!");
       this.formCreateService.value.user = this.user;
+      this.formCreateService.value.locations = this.locations;
       console.log(this.formCreateService.value);
     }
+  }
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Datos incorrectos!',
+      subTitle: 'Por favor, revise los datos, y busque nuevamente.',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
